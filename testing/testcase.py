eb2ab2139a52ce41eb41aae23888c5541110949a
Sehar Ahmed <sahmed@experlabs.com>
2024-10-02 14:25:18 -0400

    Commit these changes

---
+++ testing/testcase.py
@@ -0,0 +1,17 @@
+'''
+Created on 02-Oct-2024
+
+@author: Sehar
+'''
+def func(n):
+    if n ==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return (func(n-2)+func(n-1))
+    
+for x in range(10):
+    print(func(x), end = ", ")
+
+(func(10))
