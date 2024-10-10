6dc8805a00c2f5885a53f37c3dfe2c9953883855
Sehar Ahmed <sahmed@experlabs.com>
2024-10-10 09:02:48 -0400

    added another

--- testing/testcase.py
+++ testing/testcase.py
@@ -23,3 +23,21 @@ def func2(n):
         return "naaayyy"
 print(func2("b"))
 
+def missing_number(n):
+    
+    binary_number=bin(n)[2:]
+    #print(binary_number)
+    
+    if "0" not in binary_number:
+        return 0
+    count =0
+    i=1
+    for x in range(len(binary_number)):
+        if binary_number[x] == "1":
+            while (x+ i) < len(binary_number) and binary_number[x+i]== "0":
+                count+=1
+                i+=1
+            return count
+"""print(missing_number((9)))
+print(missing_number((529)))"""
+print(missing_number((1)))
