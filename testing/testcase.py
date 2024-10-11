fc94e1414c9c93d33d407b2436b5532b4487c700
Sehar Ahmed <ahmedsehar1@gmail.com>
2024-10-11 16:05:55 -0400

    updated commitments made

---
+++ testing/testcase.py
@@ -0,0 +1,26 @@
+'''
+Created on 02-Oct-2024
+
+@author: Sehar
+'''
+def func(n):
+    if n ==0:
+        return 0
+    elif n==1:
+        return 1 #this is done
+    else:
+        return (func(n-2)+func(n-1))
+    
+for x in range(10):
+    print(func(x), end = ", ")
+
+(func(10))
+
+def func2(n):
+    if n in "abcde":
+        return "yeeeey"
+    else:
+        return "naaayyy"
+print(func2("b"))
+
+print("now she is not annoying")
